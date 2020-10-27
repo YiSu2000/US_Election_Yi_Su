14ea3cd6a92b3a57cc46b8b123c0583a3317b9fe
Yi Su <jordan.su@mail.utoronto.ca>
2020-10-27 17:33:46 -0400

    A4C1

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
-# US_Election_Yi_Su
+# Forecast_US_Election_YS6431
+
+This repo contains code and data for forecasting the US 2020 presidential election. It was created by Yi Su. The purpose is to create a report that summarises the results of a statistical model that we built. Some data is unable to be shared publicly. We detail how to get that below. The sections of this repo are: inputs, outputs, scripts.
+
+Inputs contain data that are unchanged from their original. Since they are not to be shared publicly, not input has been pushed to Github.
+We use two datasets: 
+
+- [Survey data - Survey Data thankfully obtained from: 'https://www.voterstudygroup.org/publication/nationscape-data-set'.]
+
+- [ACS data - America Community Survey dataset from: https://usa.ipums.org/usa/index.shtml]
+
+Outputs the report and supporting material.
+
+- US-Election_Yi Su.Rmd and .pdf, 
+- model3.rds, resultant model
+
+Scripts contain R scripts that take inputs and outputs and produce outputs. These are:
+
+- 01_data_cleaning.R
+- 02_data_preparation.R
---
+++ scripts/01-data_cleaning-post-strat.R
@@ -0,0 +1,41 @@
+#### Preamble ####
+# Purpose: Prepare and clean the survey data downloaded from [...UPDATE ME!!!!!]
+# Author: Rohan Alexander and Sam Caetano [CHANGE THIS TO YOUR NAME!!!!]
+# Data: 22 October 2020
+# Contact: rohan.alexander@utoronto.ca [PROBABLY CHANGE THIS ALSO!!!!]
+# License: MIT
+# Pre-requisites: 
+# - Need to have downloaded the ACS data and saved it to inputs/data
+# - Don't forget to gitignore it!
+
+
+#### Workspace setup ####
+library(haven)
+library(tidyverse)
+# Read in the raw data. 
+raw_data <- read_dta("inputs/data/usa_00002.dta"
+                     )
+# Add the labels
+raw_data <- labelled::to_factor(raw_data)
+
+# Just keep some variables that may be of interest (change 
+# this depending on your interests)
+names(raw_data)
+
+reduced_data <- 
+  raw_data %>% 
+  select(stateicp,
+         sex, 
+         age, 
+         race, 
+         hhincome,
+         educ,
+         empstat)
+rm(raw_data)
+write_csv(reduced_data, "reduced_data_post_strat_1.csv")         
+
+#### What's next? ####
+
+
+
+         
